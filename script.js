d13e9a9a901c85935a6eb18bec187617d5958145
Abdullah khan <akhan283785@gmail.com>
2024-12-19 01:34:07 +0530

    try

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-//your JS code here. If required.
+let line = document.querySelector("#line");
+let count = 0;
+
+let rotater = setInterval(() => {
+  line.style.transform = `rotate(${count}deg)`;
+  count += 2;
+  if (count > 180) clearInterval(rotater);
+}, 20);
